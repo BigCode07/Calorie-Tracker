402e538673a62a6101b8c0e6ae2273afba1d4da3
BigCode07 <bigcode07@gmail.com>
2025-01-04 02:20:00 -0300

    Se instalo uuid para cargar Id al registro

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,52 @@
+import { useState, ChangeEvent, FormEvent, Dispatch } from "react";
+import { v4 as uuidv4 } from "uuid";
 import { categories } from "../data/categories";
+import { Activity } from "../types/index";
+import { ActivityActions } from "../reducers/activityReducer";
 
-export const Form = () => {
+type FormProps = {
+  dispatch: Dispatch<ActivityActions>;
+};
+
+const initialState: Activity = {
+  id: uuidv4(),
+  category: 1,
+  name: "",
+  calories: 0,
+};
+
+export const Form = ({ dispatch }: FormProps) => {
+  const [activity, setActivity] = useState<Activity>(initialState);
+
+  const handleChange = (
+    e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>
+  ) => {
+    const isNumberField = ["category", "calories"].includes(e.target.id);
+
+    setActivity({
+      ...activity,
+      [e.target.id]: isNumberField ? +e.target.value : e.target.value,
+    });
+  };
+
+  const isValidActivy = () => {
+    const { name, calories } = activity;
+    console.log(name.trim() !== "" && calories > 0);
+    return name.trim() !== "" && calories > 0;
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    dispatch({ type: "SaveActiviy", payload: { newActivity: activity } });
+
+    setActivity({ ...initialState, id: uuidv4() });
+  };
   return (
-    <form action="" className=" space-y-5 bg-white shadow p-10 rounded-lg">
+    <form
+      action=""
+      className=" space-y-5 bg-white shadow p-10 rounded-lg"
+      onSubmit={handleSubmit}
+    >
       <div className="grid grid-cols-1 gap-3">
         <label htmlFor="category" className=" font-bold">
           Categoria:
@@ -11,6 +55,8 @@ export const Form = () => {
           className=" border border-slate-300 p-2 rounded-lg w-full bg-white"
           name=""
           id="category"
+          value={activity.category}
+          onChange={handleChange}
         >
           {categories.map((category) => (
             <option key={category.id} value={category.id}>
@@ -20,14 +66,16 @@ export const Form = () => {
         </select>
       </div>
       <div className=" grid grid-cols-1 gap-3">
-        <label htmlFor="activity" className="font-bold">
+        <label htmlFor="name" className="font-bold">
           Actividad:
         </label>
         <input
           type="text"
-          id="activiy"
+          id="name"
           className=" border border-slate-300 p-2 rounded-lg"
           placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicletas"
+          value={activity.name}
+          onChange={handleChange}
         />
       </div>
       <div className=" grid grid-cols-1 gap-3">
@@ -39,12 +87,15 @@ export const Form = () => {
           id="calories"
           className=" border border-slate-300 p-2 rounded-lg"
           placeholder="Calorias. Ej. 300 o 500 "
+          value={activity.calories}
+          onChange={handleChange}
         />
       </div>
       <input
         type="submit"
-        className=" bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
-        value="Guardar Comida o Guardar Ejercicio"
+        className=" bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer disabled:opacity-10"
+        value={activity.category === 1 ? "Guardar Comida" : "Guardar Ejercicio"}
+        disabled={!isValidActivy()}
       />
     </form>
   );
